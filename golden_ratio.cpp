78396b03150fe2550314de4846a7fbdaf4244295
theo tanase <th.tanase@gmail.com>
2014-10-27 17:02:03 +0200

    main

--- golden_ratio.cpp
+++ golden_ratio.cpp
@@ -3,4 +3,19 @@ mosix
 golden ratio
 */
 #include <stdio.h>
+#include "gr.h"
 
+int main() {
+
+	int precision;
+
+	double result;
+
+	fscanf()
+
+	result = gr(precision);
+
+	printf("Golden ratio, with %d precision = %lf\n", precision, result );
+
+	return 0;
+}

b8cfc6c5cc9d32e10668b8f6668ed08e10797c26
CristinaGrosu01 <crystyna_smilegirl@yahoo.co.uk>
2014-10-27 17:17:46 +0200

    added comments

--- calcgr.h
+++ calcgr.h
@@ -1,3 +1,6 @@
 #include <stdio.h>
 
+/*Function used to calculate the golden ratio having N precision*/
+/*Parameters : n - integer - represents precision number*/
+
 double calcgr(int n);
--- golden_ratio.cpp
+++ golden_ratio.cpp
@@ -6,21 +6,28 @@ golden ratio
 #include "calcgr.h"
 #include "gr.h"
 
+/* implementation of the function defined in file 'calcgr.h' */
+
 double calcgr(int n) {
  double v = 1.618033988749L;
  return v;
 }
 
+/* The core part of the application */
+
 int main() {
 
+	//declare variables
 	int precision;
-
 	double result;
 
-	fscanf()
+	//read precision value
+	fscanf();
 
+	//calculate the golden ration number having N precision
 	result = gr(precision);
 
+	//display the results in a formatted way
 	printf("Golden ratio, with %d precision = %lf\n", precision, result );
 
 	return 0;

f3dc94f1e198f370995f6ab5e9ad8ec66e9e8e19
Anda <anda.nenu@gmail.com>
2014-10-27 17:07:27 +0200

    from andanenu

--- golden_ratio.cpp
+++ golden_ratio.cpp
@@ -4,3 +4,14 @@ golden ratio
 */
 #include <stdio.h>
 
+void readFromFile(int &a, &b, char fileName[]){
+	FILE *file;
+	file = fopen(fileName, "rt");
+	if (fp == NULL){
+		perror("Error while opening the file.\n");
+		exit(EXIT_FAILURE);
+	}
+	fscanf(file, "%d", &a);
+	fscanf(file,"%d", &b);
+	fclose(file);
+} 
